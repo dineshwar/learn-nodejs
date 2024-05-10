0598114d22f89744fa13d3b06ad08b4b11bac600
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-05-10 22:47:46 +0530

    Update main.js

--- multi-threading/main.js
+++ multi-threading/main.js
@@ -3,7 +3,6 @@ const { Worker } = require("worker_threads");
 function doSomethingCPUIntensive(name) {
   return new Promise((resolve, reject) => {
     const worker = new Worker("./sub.js", { workerData: { name } });
-
     worker.on("message", resolve);
     worker.on("error", reject);
     worker.on("exit", (code) => {
@@ -13,6 +12,7 @@ function doSomethingCPUIntensive(name) {
     });
   });
 }
+
 (async () => {
   try {
     const result = await doSomethingCPUIntensive("Dineshwar");
