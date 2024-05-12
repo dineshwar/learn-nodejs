d0cc2a60881b694d083595b161127bca8cbafe0f
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-05-12 15:37:29 +0530

    Event loop 1

---
+++ multi-threading/event-loop-phases.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+setImmediate(() => console.log(1));
+
+Promise.resolve(() => console.log(2));
+
+process.nextTick(() => console.log(3));
+
+fs.readFile(__filename, () => {
+  console.log(4);
+  setTimeout(() => console.log(5));
+  setImmediate(() => console.log(6));
+});
