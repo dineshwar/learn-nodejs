3f7a44acf5456696a2a6632e3c303c48beb0e585
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-05-15 23:09:03 +0530

    Event loop 7

--- event-loop/event-loop2.js
+++ event-loop/event-loop2.js
@@ -1,4 +1,7 @@
 const sleep_st = (t) => new Promise((r) => setTimeout(r, t));
 const sleep_lm = () => new Promise((r) => setImmediate(r));
 
-
+(async () => {
+  setImmediate(() => console.log(1));
+  console.log(2);
+})();
