e35718d2be1d5e084f37b7e89f2fa5bd3124e968
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-05-22 21:54:25 +0530

    FIle system

---
+++ read_line/read_line.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', (line) => {
+    console.log(line);
+});
+
+rl.once('close', () => {
+     // end of input
+ });
+
